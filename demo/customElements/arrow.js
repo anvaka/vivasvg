acab6e6f9179bf85233830a807156b9283a1a5c4
anvaka <anvaka@gmail.com>
2014-02-27 03:32:21 -0800

    Adding marker-end

--- demo/customElements/arrow.js
+++ demo/customElements/arrow.js
@@ -20,6 +20,7 @@ Arrow.prototype._appendToDom = function (parentDom) {
 };
 
 function compileMarkup(markup, model, arrow) {
+  addArrowTriangle(arrow);
   var path = vivasvg.svg('path');
   var bindingParser = vivasvg.bindingParser(model);
 
@@ -36,6 +37,15 @@ function compileMarkup(markup, model, arrow) {
     var dest = to.provide();
     path.setAttributeNS(null, 'd', 'M' + source.x +',' + source.y + 'L' + dest.x + ',' + dest.y);
   }
+  path.setAttributeNS(null, 'marker-end', 'url(#ArrowTriangle)');
 
   return path;
+}
+
+function addArrowTriangle(arrow) {
+  var ownerDocument = arrow.getOwnerDocument(arrow);
+  if (ownerDocument && !ownerDocument.ArrowAugmented) {
+    ownerDocument.addDef('<marker id="ArrowTriangle" viewBox="0 0 10 10" refX="8" refY="5" markerUnits="strokeWidth" markerWidth="10" markerHeight="5" orient="auto" style="fill: deepskyblue"><path d="M 0 0 L 10 5 L 0 10 z"></path></marker>');
+    ownerDocument.ArrowAugmented = true; // todo: should be better way
+  }
 }
