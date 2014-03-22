70aefec0947c4fd14070fa413ddd688b5bb8f3c8
anvaka <anvaka@gmail.com>
2014-03-22 00:42:26 -0700

    Added small profile script

---
+++ experiments/v0.2/demo/items/profile.sh
@@ -0,0 +1,1 @@
+/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary --no-sandbox --js-flags="--trace-opt --trace-bailout --trace-deop" --user-data-dir=/Users/anvaka/chromeDebug
