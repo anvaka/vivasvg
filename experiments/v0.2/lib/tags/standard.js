7f390a3e699d384861fa933751fbc76eb02e87c0
anvaka <anvaka@gmail.com>
2014-03-21 22:38:17 -0700

    Removed debugger

--- experiments/v0.2/lib/tags/standard.js
+++ experiments/v0.2/lib/tags/standard.js
@@ -6,7 +6,6 @@ createTag('circle', function (virtual) {
       create: function () {
         var circle = virtual.domNode.cloneNode(false);
 
-        debugger;
         virtual.bind('cx', model, function (newValue) { circle.cx.baseVal.value = newValue; });
         virtual.bind('cy', model, function (newValue) { circle.cy.baseVal.value = newValue; });
 
@@ -23,7 +22,6 @@ createTag('items', function (virtual){
         var g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
         var template = virtual.children[0];
 
-        debugger;
         virtual.bind('source', model, function (newValue) {
           for (var i = 0; i < newValue.length; ++i) {
             var child = template(newValue[i]).create();
