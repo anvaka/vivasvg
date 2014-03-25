eec4b06df0964501f46d4ee0e4616b99b83b9974
anvaka <anvaka@gmail.com>
2014-03-25 07:55:56 -0700

    Removed global in demo, no more color binding

--- experiments/v0.2/demo/items/bundle.js
+++ experiments/v0.2/demo/items/bundle.js
@@ -10,14 +10,11 @@ function createViewModel(count) {
   var viewModels = [];
   for (var i = 0; i < count; ++i) {
     var xSpeed = Math.random() * 10 - 5;
-    var color = Math.round(0.5 * xSpeed + 5);
-    color = '#' + ((color << 32) + (color << 16) + color);
     var ball = {
       x: Math.random() * 640,
       y: Math.random() * 480,
       dx: xSpeed,
       dy: Math.random() * 10 - 5,
-      fill: color
     };
 
     viewModels.push(vivasvg.viewModel(ball));
@@ -33,7 +30,7 @@ function createViewModel(count) {
     requestAnimationFrame(render);
 
     for (var i = 0; i < viewModels.length; ++i) {
-      model = viewModels[i];
+      var model = viewModels[i];
       model.x += model.dx; if (model.x < 0 || model.x > 640 ) { model.dx *= -1; model.x += model.dx; }
       model.y += model.dy; if (model.y < 0 || model.y > 480 ) { model.dy *= -1; model.y += model.dy; }
       model.invalidate('x', 'y');
